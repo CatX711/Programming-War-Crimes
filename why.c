1c8f5e1e1eeebddb6a69f0100e6966acd7312aa1
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-22 15:43:49 +0100

    Update why.c

--- why.c
+++ why.c
@@ -1,1 +1,15 @@
-# include <stdio.h>
+#include <stdio.h>
+
+#define H "H"
+#define e "e"
+#define l "l"
+#define o "o"
+#define comma ","
+#define space " "
+#define world "world"
+
+
+int main(){
+  printf(H, e, l, l, o, comma, space, world "\n");
+  return 0;
+}
