6448b46f0f2a3b954a15f3b68d350b14c549c4d3
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-22 20:08:50 +0100

    Add files via upload

---
+++ slang_lang.c
@@ -0,0 +1,32 @@
+#define rn ;
+#define cap false
+#define no !
+#define sussin ?
+#define fr long
+#define chief() main()
+#define bouta &
+#define finna =
+#define mf *
+#define bussin :
+#define be ==
+#define yikes break
+#define vibin --
+#define ongod ++
+
+#include <stdio.h>
+
+int chief() {
+    fr int c rn
+    scanf("%d", bouta c) rn
+    fr fr int mf p rn
+    c > 3 sussin p finna bouta c[0] bussin p finna NULL rn
+    while (no cap) {
+        if (p be cap){
+        yikes rn
+        }
+        printf("%p, %d\n", p, c) rn
+        p vibin rn // careful dont use mf or p or it segfaults fr fr
+    }
+
+    return 0 rn
+}   
