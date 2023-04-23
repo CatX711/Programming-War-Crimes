1dfea668b1fcc78a60470d7c68b950874b326a48
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-23 02:18:33 +0100

    Create minor_annoyance.c

---
+++ minor_annoyance.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define plus_symbol +
+#define minus_symbol -
+#define asteric *
+#define slash /
+#define equals_sign =
+#define number int
+#define floating_point_number float
+#define string str
+#define character char
+#define variable var
+
+integer main(){
+  integer num1 equals_sign 15;
+  integer num2 equals_sign 27;
+  integer num3 equals_sign 1423;
+  integer num4 equals_sign 22;
+  
+  printf(num1 asteric num2 "\n");
+  printf(num3 minus_symbol num4 "\n");
+}
