a10201ff15f21815d7fd3baa14fa60c39e7c887d
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-04-23 01:48:57 +0100

    Update MY_BRAIN_HURTS.cpp

--- MY_BRAIN_HURTS.cpp
+++ MY_BRAIN_HURTS.cpp
@@ -4,6 +4,6 @@ int main() {
   int x[3] = {1, 2, 3};
   
   std::cout << x[1] << "\n";
-  std::cout << 1[x] << "\n";
+  std::cout << 1[x] << "\n"; // wait thats the same thing right 😭
   return 0;
 }
